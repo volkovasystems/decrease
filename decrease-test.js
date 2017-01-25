fd81879afa88d2755d59f3e4301ed688e824bdcd
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-25 16:50:29 +0800

    Added test.

---
+++ decrease-test.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const decrease = require( "./decrease.js" );
+
+let array = [ 1, 2, 3 ];
+
+let result = decrease( array,
+	function onDecrease( oldArray, currentValue, index, array ){
+
+		return 1;
+
+	}, [ ] );
+
+console.log( "Result", result );
