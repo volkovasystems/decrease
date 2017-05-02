2534dd1e1bf7e0bc015ef76ed08cd25862191cd4
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-02 13:50:02 +0800

    Updated test.

--- decrease-test.js
+++ decrease-test.js
@@ -1,11 +1,11 @@
 "use strict";
 
+const assert = require( "assert" );
 const decrease = require( "./decrease.js" );
 
 let array = [ 1, 2, 3 ];
-
 let result = decrease( array );
+assert.ok( result );
+assert.ok( decrease( ) );
 
-console.log( "Result", result );
-
-console.log( decrease( ) );
+console.log( "ok" );
