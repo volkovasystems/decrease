8ee8b4a3a27b39e765dc8e5ae1c0c39fd084ce2e
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-06 18:08:52 +0800

    Updated server/client test.

--- decrease-test.js
+++ decrease-test.js
@@ -2,18 +2,22 @@
 const assert = require( "assert" );
 const decrease = require( "./decrease.js" );
 
-assert.deepEqual( decrease( [ null, undefined, "" ] ), [ ], "should be empty array" );
-assert.deepEqual( decrease( [ null, NaN ] ), [ ], "should be empty array" );
-assert.deepEqual( decrease( [ null, { } ] ), [ ], "should be empty array" );
-assert.deepEqual( decrease( [ null, [ ] ] ), [ ], "should be empty array" );
-assert.deepEqual( decrease( [ null, [ ], { } ] ), [ { } ], "should be array with empty object within the array" );
-assert.deepEqual( decrease( [ 1, 2, 3, null ] ), [ 1, 2, 3 ], "should have truthy values" );
-assert.deepEqual( decrease( [ null, 1, 2, 3 ] ), [ 1, 2 ], "should have truthy values" );
-assert.deepEqual( decrease( [ 1, 2, NaN, 3 ] ), [ 1, 2 ], "should have truthy values" );
-
 assert.deepEqual( decrease( [ 1, 2, 3, 4, 5 ] ), [ 1, 2, 3, 4 ], "should removed element '5'" );
 
-let test = decrease( [ 1, 2, 3, 4, 5 ], function flatten( previous, current ){ return previous.concat( current ); }, [ ] );
-assert.deepEqual( test, [ 1, 2, 3, 4, 5 ], "should be equal" );
+assert.deepEqual( decrease( [ 1, 2, 3, null ] ), [ 1, 2, 3 ], "should have truthy values" );
+
+assert.deepEqual( decrease( [ null, 1, 2, 3 ] ), [ 1, 2 ], "should have truthy values" );
+
+assert.deepEqual( decrease( [ 1, 2, NaN, 3 ] ), [ 1, 2 ], "should have truthy values" );
+
+let test = decrease( [ 1, 2, 3 ], function sum( previous, current ){
+	return previous + current;
+} );
+assert.deepEqual( test, [ 7 ], "should be deeply equal" );
+
+let test2 = decrease( [ 1, 2, 3 ], function sum( previous, current ){
+	return previous + current;
+}, 0 );
+assert.deepEqual( test2, [ 6 ], "should be deeply equal" );
 
 console.log( "ok" );
